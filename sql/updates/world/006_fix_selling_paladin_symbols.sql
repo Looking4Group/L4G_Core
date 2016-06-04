e3c7192928668362e47990f0d808bb0f863131fc
AbaddonL4G <sapphire.gaming@live.com>
2016-06-04 23:12:35 +0200

    DB: fix selling paladin symbols
    fix selling paladin symbols 21177, 17033

---
+++ sql/updates/world/006_fix_selling_paladin_symbols.sql
@@ -0,0 +1,4 @@
+-- Symbol of Divinity
+UPDATE `item_template` SET `SellPrice`='0' WHERE `entry` IN ('17033'); -- 500
+-- Symbol of Kings
+UPDATE `item_template` SET `SellPrice`='0' WHERE `entry` IN ('21177'); -- 37
