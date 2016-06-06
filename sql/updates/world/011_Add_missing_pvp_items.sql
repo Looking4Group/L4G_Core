a19d8cd3e2b3eb763071e8681459070c33e3388e
AbaddonL4G <sapphire.gaming@live.com>
2016-06-06 20:49:24 +0200

    DB/Vendor: Add missing PvP Neck
    Pendant of Dominance, Pendant of Triumph
    now ingame

---
+++ sql/updates/world/011_Add_missing_pvp_items.sql
@@ -0,0 +1,7 @@
+-- add missing items http://www.wowhead.com/item=28245 http://de.wowhead.com/item=28244 to pvp vendor
+
+INSERT INTO `npc_vendor` VALUES ('12793','28244','0','0','127');
+INSERT INTO `npc_vendor` VALUES ('12793','28245','0','0','127');
+
+INSERT INTO `npc_vendor` VALUES ('12781','28244','0','0','127');
+INSERT INTO `npc_vendor` VALUES ('12781','28245','0','0','127');
