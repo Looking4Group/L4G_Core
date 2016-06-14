7d91688344f5c210f22b7a313f287502f81b2f7a
AnonXS <fetzi-d@web.de>
2016-06-14 14:36:38 +0200

    Update SpellEffects.cpp
    https://bitbucket.org/looking4group_b2tbc/looking4group/issues/490/dark-portal-npc-fighting

--- src/game/SpellEffects.cpp
+++ src/game/SpellEffects.cpp
@@ -802,6 +802,15 @@ void Spell::EffectDummy(uint32 i)
                     m_caster->CastSpell(unitTarget, spell_list[urand(0, 5)], true);
                     return;
                 }
+                case 32785:                                 // Infernal Rain
+                {
+                    if (!unitTarget || unitTarget->GetTypeId() != TYPEID_UNIT /*|| unitTarget->GetEntry() != 19215*/)
+                        return;
+
+                    // Summon Infernal Siegebreaker
+                    m_caster->SummonCreature(18946, unitTarget->GetPositionX(), unitTarget->GetPositionY(), unitTarget->GetPositionZ(), 0.0f, TEMPSUMMON_CORPSE_TIMED_DESPAWN, 60000);
+                    return;
+                }
                 case 46292:                                 // Cataclysm Breath
                 {
                     // Cataclysm Spells
