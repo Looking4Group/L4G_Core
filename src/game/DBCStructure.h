70a2fd782e4ee8cd022092cc8b3103f0d2956e9c
Gekirin <pbeitelmann@yahoo.de>
2016-09-29 21:32:29 +0200

    DBCStructure: comments added

--- src/game/DBCStructure.h
+++ src/game/DBCStructure.h
@@ -722,11 +722,11 @@ struct SpellEntry
     int32     EquippedItemSubClassMask;                     // 63 (mask)
     int32     EquippedItemInventoryTypeMask;                // 64 (mask)
     uint32    Effect[3];                                    // 65-67
-    int32     EffectDieSides[3];                            // 68-70
+    int32     EffectDieSides[3];                            // 68-70 variable damage
     uint32    EffectBaseDice[3];                            // 71-73
     float     EffectDicePerLevel[3];                        // 74-76
     float     EffectRealPointsPerLevel[3];                  // 77-79
-    int32     EffectBasePoints[3];                          // 80-82 (don't must be used in spell/auras explicitly, must be used cached Spell::m_currentBasePoints)
+    int32     EffectBasePoints[3];                          // 80-82 absolute damage (don't must be used in spell/auras explicitly, must be used cached Spell::m_currentBasePoints)
     uint32    EffectMechanic[3];                            // 83-85
     uint32    EffectImplicitTargetA[3];                     // 86-88
     uint32    EffectImplicitTargetB[3];                     // 89-91
