456422856cbd2c5b1b8eb91e0158a4e18b70817e
Anteara <Anteara@users.noreply.github.com>
2016-10-09 09:44:41 +1100

    removed change of spellmgr

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3721,11 +3721,7 @@ void SpellMgr::LoadSpellCustomAttr()
                 break;
             case 37029: //Kael'Thas Telonicus: Remove DR from remote toy by setting mechanic to 0
                 spellInfo->Mechanic = 0;
-                break;
-            case 30898: //Prince Phase 3 SWP
-                spellInfo->Targets = TARGET_UNIT_TARGET_ENEMY;
-                spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_TARGET_ENEMY;
-                break;       
+                break;    
             case 30834: //Prince infernals targeting
                 spellInfo->Targets = TARGET_UNIT_TARGET_ANY;
                 spellInfo->EffectImplicitTargetA[0] = TARGET_UNIT_TARGET_ANY;
