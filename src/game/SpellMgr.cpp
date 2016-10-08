5e3667807f345369b1bc15308574665e9eddf97a
Gekirin <pbeitelmann@yahoo.de>
2016-10-08 18:27:28 +0200

    prince ifnernals target change

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3721,7 +3721,10 @@ void SpellMgr::LoadSpellCustomAttr()
                 break;
             case 37029: //Kael'Thas Telonicus: Remove DR from remote toy by setting mechanic to 0
                 spellInfo->Mechanic = 0;
-                break;                
+                break;
+            case 30834: //Prince infernals targeting
+                spellInfo->Targets = 6;
+                break;
             default:
                 break;
         }
