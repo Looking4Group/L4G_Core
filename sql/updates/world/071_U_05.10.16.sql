b547216e84d80f46cdf8a7a0d309843f9594aede
AnonXS <fetzi-d@web.de>
2016-10-08 16:55:04 +0200

    Swamprat Guards

--- sql/updates/world/071_U_05.10.16.sql
+++ sql/updates/world/071_U_05.10.16.sql
@@ -772,3 +772,61 @@ INSERT INTO `game_event_creature` VALUES
 
 -- mob_ember_of_alar
 UPDATE `creature_template` SET `speed`=4 WHERE `entry` = 19551;
+
+-- ----------------------------------------------------------
+-- https://github.com/Looking4Group/L4G_Core/issues/1301
+-- Swamprat Guards not patrolling.
+-- ----------------------------------------------------------
+
+ -- Source for numbers: Corona DB
+UPDATE `creature_template` SET `mindmg`=430, `maxdmg`=522, `AIName` = 'EventAI' WHERE  `entry` = 18910;
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 18910;
+INSERT INTO `creature_ai_scripts` VALUES
+('1891001','18910','0','0','100','1','5000','7000','7000','10000','11','12169','0','0','0','0','0','0','0','0','0','0','Swamprat Guard - Cast Shield Block'); 
+-- Source for waypoints: https://searchcode.com/file/115778080/sql/old/4.3.4/TDB6_to_TDB7_updates/world/012_2014_07_30_07_world_waypoints.sql#
+-- Pathing for Swamprat Guard Entry: 18910
+SET @GUID := 67907;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=75.29373,`position_y`=5261.419,`position_z`=23.08392 WHERE `guid`=@GUID;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=0,`position_x`=75.29373,`position_y`=5261.419,`position_z`=23.08392 WHERE `guid`=67906;
+DELETE FROM `creature_addon` WHERE `guid`=@GUID;
+INSERT INTO `creature_addon` (`guid`,`path_id`,`bytes2`,`mount`,`auras`) VALUES (@GUID,@GUID,257,0, '5301');
+DELETE FROM `waypoint_data` WHERE `id`=@GUID;
+INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
+(@GUID,1,75.29373,5261.419,23.08392,0,0,0,100,0),
+(@GUID,2,75.29796,5274.807,23.04233,0,0,0,100,0),
+(@GUID,3,82.62872,5302.216,22.98195,0,0,0,100,0),
+(@GUID,4,83.02105,5314.273,22.13724,0,0,0,100,0),
+(@GUID,5,82.34508,5302.751,22.97913,0,0,0,100,0),
+(@GUID,6,75.29796,5274.807,23.04233,0,0,0,100,0),
+(@GUID,7,75.29373,5261.419,23.08392,0,0,0,100,0),
+(@GUID,8,75.86388,5247.502,23.12924,0,0,0,100,0),
+(@GUID,9,79.21181,5240.832,23.16174,0,0,0,100,0),
+(@GUID,10,87.85194,5229.543,22.38807,0,0,0,100,0),
+(@GUID,11,79.21181,5240.832,23.16174,0,0,0,100,0),
+(@GUID,12,75.86388,5247.502,23.12924,0,0,0,100,0);
+ 
+-- Add Swamprat Guard formation
+DELETE FROM `creature_formations` WHERE `leaderGUID`=67907;
+INSERT INTO `creature_formations` (`leaderGUID`,`memberGUID`,`dist`,`angle`,`groupAI`)VALUES
+(67907,67907,0,0,2),(67907,67906,3,0,2);
+ 
+-- Pathing for Swamprat Guard Entry: 18910
+SET @GUID := 67910;
+UPDATE `creature` SET `spawndist`=0,`MovementType`=2,`position_x`=104.3016,`position_y`=5221.673,`position_z`=22.33099 WHERE `guid`=@GUID;
+DELETE FROM `creature_addon` WHERE `guid`=@GUID;
+INSERT INTO `creature_addon` (`guid`,`path_id`,`bytes2`,`mount`,`auras`) VALUES (@GUID,@GUID,257,0, '5301');
+DELETE FROM `waypoint_data` WHERE `id`=@GUID;
+INSERT INTO `waypoint_data` (`id`,`point`,`position_x`,`position_y`,`position_z`,`delay`,`move_type`,`action`,`action_chance`,`wpguid`) VALUES
+(@GUID,1,104.3016,5221.673,22.33099,0,0,0,100,0),
+(@GUID,2,98.48994,5217.889,22.21583,0,0,0,100,0),
+(@GUID,3,97.17163,5209.297,21.69117,0,0,0,100,0),
+(@GUID,4,82.73155,5204.081,22.44019,0,0,0,100,0),
+(@GUID,5,85.2154,5189.0009,21.4521,0,0,0,100,0),
+(@GUID,6,90.1913,5186.492,21.33686,0,0,0,100,0),
+(@GUID,7,92.36255,5170.327,22.81916,0,0,0,100,0),
+(@GUID,8,104.2373,5163.936,22.91181,0,0,0,100,0),
+(@GUID,9,108.7816,5178.44,22.09893,0,0,0,100,0),
+(@GUID,10,121.7926,5183.15,24.04241,0,0,0,100,0),
+(@GUID,11,117.1341,5191.477,23.33294,0,0,0,100,0),
+(@GUID,12,118.593,5209.887,22.58294,0,0,0,100,0),
+(@GUID,13,114.1548,5216.103,23.23504,0,0,0,100,0);
