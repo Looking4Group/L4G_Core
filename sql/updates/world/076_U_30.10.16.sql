d7e972d0550b9d10782b3dbaef4b04f4dd74d46d
AnonXS <fetzi-d@web.de>
2016-10-31 22:01:49 +0100

    Serpentshrine Sporebat
    hacky charge fix

--- sql/updates/world/076_U_30.10.16.sql
+++ sql/updates/world/076_U_30.10.16.sql
@@ -31,3 +31,14 @@ INSERT INTO `creature_ai_scripts` VALUES
 DELETE FROM `creature_loot_template` WHERE `entry` = 23008 AND `item` = 22530;
 INSERT INTO `creature_loot_template` VALUES 
 (23008, 22530, 1.0, 0, 1, 1, 7, 333, 1);
+
+-- Serpentshrine Sporebat 21246
+-- 107150, 214300 Leben haben Spore Burst Charge 10-15 secs melee for 3000 on cloth. hacky charge fix
+-- By 1313moridin (1,661 – 13·34) on 2007/04/30 (Patch 2.0.12) they only have alittle over 200k hp
+-- http://wowwiki.wikia.com/wiki/Serpentshrine_Sporebat?direction=next&oldid=768437 , http://www.wowhead.com/npc=21246/serpentshrine-sporebat#comments
+UPDATE `creature_template` SET `speed`='1.48',`mindmg`='3000',`maxdmg`='5000',`baseattacktime`='1000',`mechanic_immune_mask`='0' WHERE `entry` = 21246; -- 1874 3809 2000 -- 2500 3500 7,649 - 9,084
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 21246;
+INSERT INTO `creature_ai_scripts` VALUES
+('2124601','21246','9','0','50','2','6','30','0','0','11','38461','1','0','11','40602','1','0','0','0','0','0','Serpentshrine Sporebat - Cast Charge on Aggro'), -- 5 22120 -- 38461
+('2124602','21246','9','0','100','3','0','5','15800','24000','11','38924','0','1','0','0','0','0','0','0','0','0','Serpentshrine Sporebat - Cast Spore Burst'),
+('2124603','21246','0','0','100','3','10000','15000','10000','15000','11','38461','4','1','11','40602','4','1','0','0','0','0','Serpentshrine Sporebat - Cast Random Charge');
