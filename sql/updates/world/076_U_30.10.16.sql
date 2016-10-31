6383f7ff4310d63fdf102e7d28a3ad3d57222086
AnonXS <fetzi-d@web.de>
2016-10-31 20:08:35 +0100

    Formula: Enchant Bracer - Greater Dodge
    missing from Ethereum Jailor 23008 loottable + AI

--- sql/updates/world/076_U_30.10.16.sql
+++ sql/updates/world/076_U_30.10.16.sql
@@ -12,3 +12,22 @@ INSERT INTO `reference_loot_template` VALUES (10006, 31875, 0, 1, 1, 1, 0, 0, 0)
 INSERT INTO `reference_loot_template` VALUES (10006, 31876, 0, 1, 1, 1, 0, 0, 0);
 INSERT INTO `reference_loot_template` VALUES (10006, 31877, 0, 1, 1, 1, 0, 0, 0);
 -- INSERT INTO `reference_loot_template` VALUES (10006, 33186, 0, 1, 1, 1, 0, 0, 0); Plans: Adamantite Weapon Chain Readd 2.3
+
+-- Ethereum Nullifier 22822
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `entry` = 22822;
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 22822;
+INSERT INTO `creature_ai_scripts` VALUES
+(2282201,22822,2,0,100,1,50,0,10000,15000,11,36513,0,1,0,0,0,0,0,0,0,0,'Ethereum Nullifier - Casts Intangible Presence');
+ 
+-- Ethereum Jailor 23008
+UPDATE `creature` SET `spawntimesecs` = 1200 WHERE `id` = 23008;
+UPDATE `creature_template` SET `baseattacktime`='1400',`AIName`='EventAI' WHERE `entry` = 23008; -- 2000
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 23008;
+INSERT INTO `creature_ai_scripts` VALUES
+(2300801,23008,2,0,100,1,50,0,10000,15000,11,36513,0,1,0,0,0,0,0,0,0,0,'Ethereum Jailor - Casts Intangible Presence'),
+(2300802,23008,9,0,50,1,0,3,10000,15000,11,35924,0,1,0,0,0,0,0,0,0,0,'Ethereum Jailor - Casts Energy Flux');
+
+-- add Formula: Enchant Bracer - Greater Dodge to Ethereum Jailor Loottable
+DELETE FROM `creature_loot_template` WHERE `entry` = 23008 AND `item` = 22530;
+INSERT INTO `creature_loot_template` VALUES 
+(23008, 22530, 1.0, 0, 1, 1, 7, 333, 1);
