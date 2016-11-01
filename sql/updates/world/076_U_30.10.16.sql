a7934cfa99f06e82a59443067a22eee5b261e775
AnonXS <fetzi-d@web.de>
2016-11-01 23:52:46 +0100

    Mennu Totem Errors

--- sql/updates/world/076_U_30.10.16.sql
+++ sql/updates/world/076_U_30.10.16.sql
@@ -309,3 +309,19 @@ INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `
 INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('50028', '31912', '0', '1', '1', '1', '0', '0', '0');
 INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('50028', '31915', '0', '1', '1', '1', '0', '0', '0');
 INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `lootcondition`, `condition_value1`, `condition_value2`) VALUES ('50028', '31916', '0', '1', '1', '1', '0', '0', '0');
+
+-- Tainted Earthgrab Totem 18176,19897 
+UPDATE creature_template SET spell1 = 20654 WHERE entry = 18176; 
+UPDATE creature_template SET spell1 = 20654 WHERE entry = 19897; 
+
+-- Tainted Stoneskin Totem 18177,19900 
+UPDATE creature_template SET spell1 = 31986 WHERE entry = 18177; 
+UPDATE creature_template SET minhealth='5', maxhealth='5', spell1 = 31986 WHERE entry = 19900; 
+
+-- Corrupted Nova Totem 18179,19899 
+UPDATE creature_template SET spell1 = 33132 WHERE entry = 18176; 
+UPDATE creature_template SET spell1 = 33132 WHERE entry = 19897; 
+
+-- Mennu's Healing Ward 20208,22322 
+UPDATE creature_template SET heroic_entry = 22322, spell1 = 34977 WHERE entry = 20208; 
+UPDATE creature_template SET spell1 = 38800 WHERE entry = 22322;
