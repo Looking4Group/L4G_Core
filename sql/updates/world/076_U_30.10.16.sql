faf1023a41fb27119256f7737dccace5136ab83b
AnonXS <fetzi-d@web.de>
2016-11-01 23:55:43 +0100

    Typo

--- sql/updates/world/076_U_30.10.16.sql
+++ sql/updates/world/076_U_30.10.16.sql
@@ -319,8 +319,8 @@ UPDATE creature_template SET spell1 = 31986 WHERE entry = 18177;
 UPDATE creature_template SET minhealth='5', maxhealth='5', spell1 = 31986 WHERE entry = 19900; 
 
 -- Corrupted Nova Totem 18179,19899 
-UPDATE creature_template SET spell1 = 33132 WHERE entry = 18176; 
-UPDATE creature_template SET spell1 = 33132 WHERE entry = 19897; 
+UPDATE creature_template SET spell1 = 33132 WHERE entry = 18179; 
+UPDATE creature_template SET spell1 = 33132 WHERE entry = 19899; 
 
 -- Mennu's Healing Ward 20208,22322 
 UPDATE creature_template SET heroic_entry = 22322, spell1 = 34977 WHERE entry = 20208; 
