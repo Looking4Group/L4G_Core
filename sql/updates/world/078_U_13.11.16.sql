50313d08c3425d6f8bc69621e36f8606aa95cb6c
AnonXS <fetzi-d@web.de>
2016-11-19 19:50:18 +0100

    OHF Patrols Nerfed Respawntime

--- sql/updates/world/078_U_13.11.16.sql
+++ sql/updates/world/078_U_13.11.16.sql
@@ -253,3 +253,6 @@ UPDATE `creature_template` SET `name` = 'Level 60',`minlevel` = '60',`maxlevel` 
 SET @GUID := 1688648; 
 DELETE FROM creature WHERE guid = @GUID;
 INSERT INTO creature VALUES (@GUID, 1000003, 1, 1, 0, 0, 16240.457031, 16291.4365, 22.9310, 1.4529, 25, 0, 0, 100000, 0, 0, 0);
+
+-- OHF Patrols Nerfed Respawntime
+UPDATE `creature` SET `spawntimesecs` = 7200 WHERE `id` IN (17840,22128);
