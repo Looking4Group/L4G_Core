8b4a2415782c57eda02bcd13ead868ba8d0865ac
AnonXS <fetzi-d@web.de>
2016-11-27 22:47:34 +0100

    visual

--- sql/updates/world/Transfer_NPC.sql
+++ sql/updates/world/Transfer_NPC.sql
@@ -1,6 +1,6 @@
 DELETE FROM creature_template WHERE entry = 1000004;
 INSERT INTO creature_template VALUES
-(1000004, 0, NULL, 12449, 0, 12449, 0, 'Transfer NPC', 'Hellfire', '', 70, 70, 100000, 100000, 0, 0, 0, 1, 35, 35, 1, 1, 0.25, 0, 0, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 0, '', 0, 3, 0, 1, 0, 0, 0, 'custom_transfer');
+(1000004, 0, NULL, 12449, 0, 12449, 0, 'Transfer NPC', 'Hellfire', '', 70, 70, 100000, 100000, 0, 0, 0, 1, 35, 35, 1, 1, 0.25, 0, 0, 0, 0, 0, 2000, 0, 0, 0, 0, 0, 0, NULL, NULL, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, 0, 0, '', 0, 3, 0, 1, 1566, 0, 0, 'custom_transfer');
  
 DELETE FROM creature WHERE guid = 16800923;
 INSERT INTO creature VALUES
