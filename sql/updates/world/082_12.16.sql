e07129f8656ead670921a8976aa69f1396a3d769
AnonXS <fetzi-d@web.de>
2016-12-28 21:32:21 +0100

    GO Text incomplete
    https://github.com/cmangos/tbc-db/blob/fff09b0654b2d03afab864d993c01458c02a5f7c/Updates/SpinOnTop/301_grom_monument.sql

--- sql/updates/world/082_12.16.sql
+++ sql/updates/world/082_12.16.sql
@@ -540,3 +540,7 @@ DELETE FROM `creature` WHERE `id` = 1;
 -- BT Pre Cont.
 DELETE FROM `creature_questrelation` WHERE `quest` = 10944;
 INSERT INTO `creature_questrelation` (`id`,`quest`) VALUES ('22820','10944');
+
+-- Fixed return lines for GO 21004 (Monument to Grom Hellscream) in Ashenvale
+-- Thanks @Phatcat for reporting
+UPDATE `page_text` SET `text` = 'Here lies Grommash Hellscream, Chieftain of the Warsong Clan$B$BIn many ways, the curse of our people began and ended with Grom.$BHis name meant \'giants heart\' in our ancient tongue. He earned that \nname a hundred-fold as he stood alone before the demon Mannoroth$B- and won our freedom with his blood.$B$BLok\'Tar ogar, big brother. May the Warsong never fade.$B$B-Thrall, Warchief of the Horde' WHERE `entry` = 2211;
