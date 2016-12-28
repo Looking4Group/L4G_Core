b50e675933aee0f50b5ff0fb6a2997faae250df5
AnonXS <fetzi-d@web.de>
2016-12-28 23:04:35 +0100

    Some blasted lands stuff
    https://github.com/cmangos/tbc-db/blob/fff09b0654b2d03afab864d993c01458c02a5f7c/Updates/SpinOnTop/117_stones_of_binding.sql

--- sql/updates/world/082_12.16.sql
+++ sql/updates/world/082_12.16.sql
@@ -561,3 +561,8 @@ UPDATE `creature_template` SET `unit_flags`=33554688 WHERE `entry` = 17060;
 -- Add stealth detection to Phantom Valets (Karazhan)
 DELETE FROM `creature_template_addon` WHERE `entry` = 16408;
 INSERT INTO `creature_template_addon` VALUES (16408, 0, 0, 0, 0, 0, 0, 0, '18950 0 18950 1');
+
+-- Stones of Binding: aligned spawntime with Servants and set default flags to 4 to make it usable by players
+UPDATE gameobject SET spawntimesecs = 450 WHERE id IN (141812,141857,141858,141859);
+UPDATE gameobject_template SET flags = 4 WHERE entry IN (141812,141857,141858,141859);
+UPDATE `creature` SET `spawntimesecs` = 450 WHERE `id`  IN (7668,7669,7670,7671);
