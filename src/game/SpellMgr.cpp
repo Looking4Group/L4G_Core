e073628680a458762445ac71a71f7233041d5f07
Pbeitelmann <pbeitelmann@yahoo.de>
2017-04-04 15:09:06 +0200

    fixed switch case (#3324)

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -3030,8 +3030,6 @@ void SpellMgr::LoadSpellCustomAttr()
             case 6297:
             // Deathfrost
             case 46579:
-            // Mana Tap
-            case 28734:
             // SW: Death
             case 32409:
             // Six Demon Bag spells
@@ -3049,10 +3047,14 @@ void SpellMgr::LoadSpellCustomAttr()
             case 28733:
                 spellInfo->AttributesCu |= SPELL_ATTR_CU_NO_SPELL_DMG_COEFF;
                 break;
-            /* WELL FEED */
+            // Mana Tap
+            case 28734:
+                spellInfo->Effect[0] = SPELL_EFFECT_POWER_BURN;
+                break;
+            // WELL FEED
             case 18191:
             case 46687:
-            /* RUMS */
+            // RUMS
             case 5257:
             case 5021:
             case 5020:
@@ -3062,7 +3064,7 @@ void SpellMgr::LoadSpellCustomAttr()
             case 25722:
             case 25037:
             case 20875:
-            /* DIFF FOOD */
+            // DIFF FOOD
             case 18193:
             case 18125:
             case 18192:
@@ -3073,7 +3075,7 @@ void SpellMgr::LoadSpellCustomAttr()
             case 22730:
                 spellInfo->AttributesCu |= SPELL_ATTR_CU_TREAT_AS_WELL_FEED;
                 break;
-            /* Scrolls - no stack */
+            // Scrolls - no stack
             case 8112:  // Spirit I
             case 8113:  // Spirit II
             case 8114:  // Spirit III
@@ -3094,11 +3096,11 @@ void SpellMgr::LoadSpellCustomAttr()
             case 8095:  // Protection III
             case 12175: // Protection IV
             case 33079: // Protection V
-            /* Other to not stack with scrolls */
+            // Other to not stack with scrolls
             case 35078: // Band of the Eternal Defender
                 spellInfo->AttributesCu |= SPELL_ATTR_CU_NO_SCROLL_STACK;
                 break;
-            /* ROGUE CUSTOM ATTRIBUTES */
+            // ROGUE CUSTOM ATTRIBUTES
             case 2094:                     // Blind
                 spellInfo->AttributesCu |= SPELL_ATTR_CU_FAKE_DELAY; // add const fake delay
                 break;
@@ -3106,7 +3108,7 @@ void SpellMgr::LoadSpellCustomAttr()
             case 6774:                     // Slice'n'Dice
                 spellInfo->AttributesEx3 |= SPELL_ATTR_EX3_NO_INITIAL_AGGRO;
                 break;
-            /* SHAMAN CUSTOM ATTRIBUTES */
+            // SHAMAN CUSTOM ATTRIBUTES
             case 2895:                      // Wrath of Air Totem - disallow weird stacking
                 spellInfo->EffectImplicitTargetA[0] = spellInfo->EffectImplicitTargetA[1] = TARGET_UNIT_CASTER;
                 spellInfo->EffectImplicitTargetB[0] = spellInfo->EffectImplicitTargetB[1] = 0;
@@ -3118,11 +3120,11 @@ void SpellMgr::LoadSpellCustomAttr()
             case 30708: // Totem of Wrath
                 spellInfo->AttributesEx |= SPELL_ATTR_EX_NO_THREAT;
                 break;
-            /* WARLOCK CUSTOM ATTRIBUTES */
+            // WARLOCK CUSTOM ATTRIBUTES
             case 27285:                     // Seed of Corruption - final boom damage
                 spellInfo->AttributesEx3 |= SPELL_ATTR_EX3_CANT_TRIGGER_PROC;
                 break;
-            /* HUNTER CUSTOM ATTRIBUTES */
+            // HUNTER CUSTOM ATTRIBUTES
             case 1543:                      // Flare no longer produces combat
                 spellInfo->speed = 0;
                 spellInfo->AttributesEx3 |= SPELL_ATTR_EX3_NO_INITIAL_AGGRO;
@@ -3146,13 +3148,13 @@ void SpellMgr::LoadSpellCustomAttr()
             case 17800:
                 spellInfo->AttributesCu |= SPELL_ATTR_CU_FAKE_DELAY;
                 break;
-            /* UNSORTED */
+            // UNSORTED
             case 26635: // Troll Racial Berserker
                 spellInfo->Effect[0] = 0;
                 spellInfo->EffectApplyAuraName[1] = SPELL_AURA_MOD_HASTE;
                 spellInfo->EffectBasePoints[1] = 5;
                 break;
-            /* Damage Corrections */
+            // Damage Corrections
             case 33627: // Rain of Fire (Pit Commander)
                 spellInfo->EffectBasePoints[0] = urand(48000, 58000); // Sure not correct WoWhead comments saying about 45 - 58k damage per tick
                 break;
