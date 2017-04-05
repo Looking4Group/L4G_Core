166e03957f0ea6c868fe12c2c25e66bc140b77a9
AnonXS <anonxs@web.de>
2017-04-05 18:32:17 +0200

    Enhance 701 AI and Script for 1062
    thx@Robustious

--- sql/updates/world/091_4_2.sql
+++ sql/updates/world/091_4_2.sql
@@ -1,3 +1,10 @@
 -- Stonegazer
 UPDATE `creature` SET `spawntimesecs` = 300 WHERE `id` = 18648; -- 2500
 
+UPDATE `creature_ai_scripts` SET `action1_param1`= 1 WHERE `id` IN (70101, 70106);
+
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 1062;
+INSERT INTO `creature_ai_scripts` VALUES 
+(106201, 1062, 9, 0, 100, 1, 0, 30, 5000, 10000, 11, 421, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Nezzliok the Dire - Cast Chain Lightning'),
+(106201, 1062, 9, 0, 100, 1, 0, 20, 8000, 16000, 11, 2610, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Nezzliok the Dire - Cast Shock');
+
