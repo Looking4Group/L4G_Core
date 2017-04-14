6af37dae27c9dd3c0c2bee559fc06c6041d995dc
honeyhoney <honeyhoney@users.noreply.github.com>
2017-04-14 16:48:55 +0100

    Delete Server First Alters (wrong folder)

--- sql/updates/world/093_Server_First_Table_Faction_Column.sql
+++
@@ -1,7 +0,0 @@
--- Add the faction column (INT) to the server first table
-ALTER TABLE `server_first`
-	ADD COLUMN `faction` INT NULL AFTER `entry`;
-
--- Remove primary key (from entry field) as we want to have multiple entries per creature due to 2 factions	
-ALTER TABLE `server_first`
-	DROP PRIMARY KEY;
