0cb13a0afe66e8b2954da8b802ce5979f2129f6e
honeyhoney <honeyhoney@users.noreply.github.com>
2017-04-14 16:51:36 +0100

    Update 002_Server_First.sql

--- sql/updates/chars/002_Server_First.sql
+++ sql/updates/chars/002_Server_First.sql
@@ -1,8 +1,8 @@
 -- server first table structure
 CREATE TABLE IF NOT EXISTS `server_first` (
-  `entry` int(11) NOT NULL COMMENT 'Team ID for server first 70. Creature ID for server first boss kill.',
-  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `character_id` int(11) DEFAULT NULL,
-  `guild_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`entry`)
+	`entry` INT(11) NOT NULL COMMENT 'Team ID for server first 70. Creature ID for server first boss kill.',
+	`faction` INT(11) NULL DEFAULT NULL,
+	`date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	`character_id` INT(11) NULL DEFAULT NULL,
+	`guild_id` INT(11) NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
