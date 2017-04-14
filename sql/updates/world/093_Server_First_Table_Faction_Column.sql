cf6fe1ada1d3caaa751dd6a2a9ebe483651991e0
honeyhoney <honeyhoney@users.noreply.github.com>
2017-04-14 16:30:48 +0100

    Alter table statement is now runnable multiple times without errors

--- sql/updates/world/093_Server_First_Table_Faction_Column.sql
+++ sql/updates/world/093_Server_First_Table_Faction_Column.sql
@@ -1,6 +1,20 @@
 -- Add the faction column (INT) to the server first table
-ALTER TABLE `server_first`
-	ADD COLUMN `faction` INT NULL AFTER `entry`;
+SELECT
+	COUNT(*)
+	INTO @exist
+FROM
+	information_schema.columns 
+WHERE
+	table_schema = database()
+	AND COLUMN_NAME = 'faction'
+	AND TABLE_NAME = 'server_first'
+LIMIT 1;
+
+SET @query = IF(@exist <= 0, 'ALTER TABLE `server_first` ADD COLUMN `faction` INT NULL AFTER `entry`', 'SELECT \'Column Exists\' status');
+
+PREPARE stmt FROM @query;
+
+EXECUTE stmt;
 
 -- Remove primary key (from entry field) as we want to have multiple entries per creature due to 2 factions	
 ALTER TABLE `server_first`
