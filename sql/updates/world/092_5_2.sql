729eae68ca9d1a990fb6b3c3bccce1d10d1ff774
AnonXS <anonxs@web.de>
2017-05-16 19:09:24 +0200

    Mitigate Infernal Abuse

--- sql/updates/world/092_5_2.sql
+++ sql/updates/world/092_5_2.sql
@@ -11,3 +11,8 @@ UPDATE `quest_template` set `RewHonorableKills` = 20, `RewMoneyMaxLevel` = 0 whe
 -- PvP Turn in Quest
 UPDATE `quest_template` SET `ReqItemId1` = 20560, `RewHonorableKills` = 15 WHERE `entry` IN (8388,8385); -- nonblizzlike 0 20 blizzlike 20560 15
 
+-- http://www.wowhead.com/spell=1122/summon-infernal
+UPDATE `creature_template` SET `AIName` = 'EventAI'  WHERE `entry` = 89;
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 89;
+INSERT INTO `creature_ai_scripts` VALUES ('8901','89','1','0','100','0','10000','10000','0','0','41','0','0','0','0','0','0','0','0','0','0','0','Infernal - Despawn OOC');
+
