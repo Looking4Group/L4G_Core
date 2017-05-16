ff139dacdb5b56e8670f109d72c833a335aeddd8
AnonXS <anonxs@web.de>
2017-05-16 12:04:58 +0200

    Reduce Aqueous Lord Damage
    https://github.com/Looking4Group/L4G_Core/issues/3635

--- sql/updates/world/092_5_2.sql
+++ sql/updates/world/092_5_2.sql
@@ -1,3 +1,6 @@
 -- Pattern: Spellstrike Pants
 UPDATE `creature_loot_template` SET `groupid` = 3 WHERE `entry` IN (18708,20657) AND `item` = 24309;
 
+-- Aqueous Lord 22878
+UPDATE `creature_template` SET `mindmg`='6938',`maxdmg`='7257' WHERE `entry` = 22878;
+
