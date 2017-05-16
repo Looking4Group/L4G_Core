708c095463d4813d13e8cdc6b7bcd988bc778f17
AnonXS <anonxs@web.de>
2017-05-16 18:59:46 +0200

    Release Daily PvP and PvP Turn in Quest
    without gold (xp is converted to gold though)

--- sql/updates/world/092_5_2.sql
+++ sql/updates/world/092_5_2.sql
@@ -4,3 +4,10 @@ UPDATE `creature_loot_template` SET `groupid` = 3 WHERE `entry` IN (18708,20657)
 -- Aqueous Lord 22878
 UPDATE `creature_template` SET `mindmg`='6938',`maxdmg`='7257' WHERE `entry` = 22878;
 
+-- Daily PvP
+UPDATE `creature` SET `spawnmask` = 1 WHERE `id` IN (15350, 15351);
+UPDATE `quest_template` set `RewHonorableKills` = 20, `RewMoneyMaxLevel` = 0 where `entry` between 11335 and 11342; -- nonblizzlike 24 155999 blizzlike 20 75900
+
+-- PvP Turn in Quest
+UPDATE `quest_template` SET `ReqItemId1` = 20560, `RewHonorableKills` = 15 WHERE `entry` IN (8388,8385); -- nonblizzlike 0 20 blizzlike 20560 15
+
