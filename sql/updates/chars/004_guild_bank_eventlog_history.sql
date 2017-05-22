4b37af6158860c0d4ea11396344bfce8448da121
Xadras <julian.neumann@xadras.net>
2017-05-22 02:42:39 +0200

    Core/Guild: Add a permanent guild bank log

---
+++ sql/updates/chars/004_guild_bank_eventlog_history.sql
@@ -0,0 +1,26 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for guild_bank_eventlog_history
+-- ----------------------------
+DROP TABLE IF EXISTS `guild_bank_eventlog_log_history`;
+CREATE TABLE `guild_bank_eventlog_history` (
+  `guildid` int(11) unsigned NOT NULL DEFAULT '0',
+  `LogGuid` int(11) unsigned NOT NULL DEFAULT '0',
+  `LogEntry` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `TabId` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `PlayerGuid` int(11) unsigned NOT NULL DEFAULT '0',
+  `ItemOrMoney` int(11) unsigned NOT NULL DEFAULT '0',
+  `ItemStackCount` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `DestTabId` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `TimeStamp` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guildid`,`LogGuid`),
+  KEY `PlayerGuid` (`PlayerGuid`),
+  KEY `LogGuid` (`LogGuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `guild_bank_eventlog_history`
+MODIFY COLUMN `LogGuid`  int(11) UNSIGNED NOT NULL AUTO_INCREMENT FIRST ,
+DROP PRIMARY KEY,
+ADD PRIMARY KEY (`LogGuid`);
