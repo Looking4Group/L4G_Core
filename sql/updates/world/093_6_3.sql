33e745b6f362672948d2833d107bb512047b07eb
AnonXS <anonxs@web.de>
2017-06-15 17:39:29 +0200

    ReGUID 12719 & 7400

--- sql/updates/world/093_6_3.sql
+++ sql/updates/world/093_6_3.sql
@@ -107,3 +107,57 @@ INSERT INTO `creature_linked_respawn` VALUES (163892, 57853);
 INSERT INTO `creature_linked_respawn` VALUES (187294, 57853);
 INSERT INTO `creature_linked_respawn` VALUES (57853, 57853);
 
+-- Reguid 12719 to 76575
+SET @GUID := 76575;
+DELETE FROM `creature` WHERE `guid` IN (@GUID,12719);
+INSERT INTO `creature` VALUES (@GUID, 24994, 530, 1, 0, 0, 12848.6, -7040.74, 18.6604, 2.20449, 300, 0, 0, 69860, 0, 0, 2);
+DELETE FROM `creature_addon` WHERE `guid` IN (@GUID,12719);
+INSERT INTO `creature_addon` VALUES (@GUID, @GUID, 0, 0, 0, 0, 0, 0, NULL);
+DELETE FROM `game_event_creature` WHERE `guid` IN (@GUID,12719);
+INSERT INTO `game_event_creature` VALUES (@GUID, 42);
+DELETE FROM `waypoint_data` WHERE `id` = @GUID;
+INSERT INTO `waypoint_data` VALUES (@GUID, 1, 12855.8, -7032.47, 18.9525, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 2, 12858.4, -7006.76, 25.2855, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 3, 12850.2, -6994.06, 32.3065, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 4, 12840.5, -6986.8, 38.0928, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 5, 12826.7, -6983.79, 43.9748, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 6, 12815.7, -6985.14, 47.3124, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 7, 12806.8, -6991.77, 47.6362, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 8, 12816, -7002.94, 47.4295, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 9, 12832.2, -7003.82, 47.4274, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 10, 12837, -7022.8, 47.4028, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 11, 12844, -7033.88, 47.8643, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 12, 12835.7, -7023.58, 47.4043, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 13, 12834.8, -7008.85, 47.4273, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 14, 12817, -7001.99, 47.4273, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 15, 12807, -6992.42, 47.6398, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 16, 12815.9, -6984.86, 47.1909, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 17, 12832.4, -6983.44, 41.6628, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 18, 12847.7, -6989.91, 34.3545, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 19, 12858.2, -7005.1, 25.8451, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 20, 12858.1, -7022.35, 20.3098, 0, 0, 0, 100, 0);
+INSERT INTO `waypoint_data` VALUES (@GUID, 21, 12848.7, -7041.35, 18.6506, 0, 0, 0, 100, 0);
+
+-- Reguid 7400 to 12719
+DELETE FROM `creature` WHERE `guid` IN (7400,12718,12719,12720,12721,12722,12723,12724,12725,12726);
+INSERT INTO `creature` VALUES (12718, 23047, 564, 1, 0, 0, 448.664, 191.274, 164.065, 0.139626, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12719, 23047, 564, 1, 0, 0, 449.336, 187.124, 164.065, 0.15708, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12720, 23047, 564, 1, 0, 0, 449.499, 182.922, 164.065, 0.139626, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12721, 23047, 564, 1, 0, 0, 448.259, 195.735, 164.065, 0.05236, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12722, 23047, 564, 1, 0, 0, 442.897, 191.007, 164.065, 0.122173, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12723, 23047, 564, 1, 0, 0, 443.344, 186.511, 164.065, 0.087266, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12724, 23047, 564, 1, 0, 0, 443.843, 182.041, 164.065, 0.122173, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12725, 23047, 564, 1, 0, 0, 442.478, 195.178, 164.065, 0.10472, 7200, 0, 0, 36200, 0, 0, 0);
+INSERT INTO `creature` VALUES (12726, 23049, 564, 1, 0, 0, 454.345, 190.091, 164.065, 3.07178, 7200, 0, 0, 369000, 0, 0, 2);
+
+DELETE FROM `creature_formations` WHERE `leaderguid` = 12726;
+INSERT INTO `creature_formations` VALUES (12726, 12718, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12719, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12720, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12721, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12722, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12723, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12724, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12725, 0, 0, 2);
+INSERT INTO `creature_formations` VALUES (12726, 12726, 0, 0, 2);
+
