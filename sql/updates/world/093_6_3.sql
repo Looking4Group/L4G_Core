6e957d9c90c71ccbc450ca74cb9ffe6785b6f389
AnonXS <anonxs@web.de>
2017-06-15 15:58:17 +0200

    Improve Incandescent Fel Spark
    Spell School Immunity
    Elemental Attacks can not be blocked (http://wowwiki.wikia.com/wiki/Vexallus, http://www.wowhead.com/npc=22323/incandescent-fel-spark#comments:id=312885:reply=44260, https://forum.nostalrius.org/viewtopic.php?f=36&t=3986)
    Updated AI with most recent ACID Script

--- sql/updates/world/093_6_3.sql
+++ sql/updates/world/093_6_3.sql
@@ -1,1 +1,7 @@
+-- Incandescent Fel Spark 22323
+UPDATE `creature_template` SET `resistance2` = '-1', `flags_extra` = `flags_extra`|262144 WHERE `entry` = 22323;
+DELETE FROM `creature_ai_scripts` WHERE `entryOrGUID` = 22323;
+INSERT INTO `creature_ai_scripts` VALUES
+('2232301','22323','0','0','100','1','0','1000','10000','11000','11','36247','1','0','0','0','0','0','0','0','0','0','Incandescent Fel Spark - Cast Fel Fireball'),
+('2232302','22323','6','0','100','0','0','0','0','0','11','44877','0','3','0','0','0','0','0','0','0','0','Incandescent Fel Spark - Cast Living Flare Master on Death');
 
