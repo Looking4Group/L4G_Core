c3f118a43485afc62a7bc8cc7778a9e0dc962f47
AnonXS <anonxs@web.de>
2017-06-20 15:30:59 +0200

    Set Midsummer Sausage to 5x Stack
    as all the other consumeables are 5x Stack too

--- sql/updates/world/093_6_4.sql
+++ sql/updates/world/093_6_4.sql
@@ -2,3 +2,6 @@
 UPDATE `creature_template` SET `resistance4` = -1 WHERE `entry` = 22309;
 UPDATE `creature_template_addon` SET `auras` = NULL WHERE `entry` = 22309;
 
+-- Midsummer Sausage 5er Stack
+UPDATE `item_template` SET `BuyCount` = 5 WHERE `entry` = 23326;
+
