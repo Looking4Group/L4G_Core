94b11cf94c74f4621b2135c0c9cecbbbd2daa4fb
AnonXS <anonxs@web.de>
2017-06-20 15:37:13 +0200

    Set Duration for Event Food
    http://www.wowhead.com/item=23211/toasted-smorc#comments:id=316032 14 days in bag.

--- sql/updates/world/093_6_4.sql
+++ sql/updates/world/093_6_4.sql
@@ -5,3 +5,6 @@ UPDATE `creature_template_addon` SET `auras` = NULL WHERE `entry` = 22309;
 -- Midsummer Sausage 5er Stack
 UPDATE `item_template` SET `BuyCount` = 5 WHERE `entry` = 23326;
 
+-- Set Duration as already set for 23215 14 days duration!
+UPDATE `item_template` SET `duration` = 1209600 WHERE `entry` IN (23327,23326,23211,23215,23246,23435);
+
