7bdc1a897011cdfe4ed0e45d7b9227c7190c1b81
anonxs <anonxs@web.de>
2017-06-29 19:07:16 +0200

    Reduce Checktimer in Nonarena
    make detection more reliable

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1468,6 +1468,7 @@ void Player::Update(uint32 update_diff, uint32 p_time)
         if (update_diff >= m_DetectInvTimer)
         {
             HandleStealthedUnitsDetection();
+            m_DetectInvTimer = InArena() ? 1000 : 2000;
         }
         else
             m_DetectInvTimer -= update_diff;
